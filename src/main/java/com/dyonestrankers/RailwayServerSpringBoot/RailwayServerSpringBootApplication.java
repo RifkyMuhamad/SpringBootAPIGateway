121c203e3a0b9efc143e1a9c25408d6284ea344b
unknown <rifkymuhamad0022@gmail.com>
2024-01-08 18:43:41 +0700

    first commit

---
+++ src/main/java/com/dyonestrankers/RailwayServerSpringBoot/RailwayServerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dyonestrankers.RailwayServerSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayServerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayServerSpringBootApplication.class, args);
+	}
+
+}
